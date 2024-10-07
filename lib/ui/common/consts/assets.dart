8d38e4b8dfa541db045314d80f8a49bf82569890
bluekmky1 <115731932+bluekmky1@users.noreply.github.com>
2024-10-08 00:39:40 +0900

    🚚 assets 파일 경로 변경

---
+++ lib/ui/common/consts/assets.dart
@@ -0,0 +1,24 @@
+class Assets {
+  // Images
+  static const String emptySpace1 = 'assets/images/empty_space.png';
+  static const String emptySpace2 = 'assets/images/empty_space2.png';
+  static const String emptySpace3 = 'assets/images/empty_space3.png';
+  static const String emptySpace4 = 'assets/images/empty_space4.png';
+
+  // Icons
+  static const String addImage = 'assets/icons/add_image.svg';
+  static const String back = 'assets/icons/back.svg';
+  static const String clear = 'assets/icons/clear.svg';
+  static const String edit = 'assets/icons/edit.svg';
+  static const String lock = 'assets/icons/lock.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String signIn = 'assets/icons/sign_in.svg';
+  static const String signOut = 'assets/icons/sign_out.svg';
+  static const String trashCan = 'assets/icons/trash_can.svg';
+  static const String userInfo = 'assets/icons/user_info.svg';
+
+  // mockImage
+  static const String book1 = 'assets/mock_images/book1.png';
+  static const String book2 = 'assets/mock_images/book2.png';
+  static const String book3 = 'assets/mock_images/book3.png';
+}
--- lib/ui/consts/assets.dart
+++
@@ -1,7 +0,0 @@
-class Assets {
-  static const String logo = 'assets/logo.png';
-  static const String logoHorizon = 'assets/logo_horizon.png';
-
-  static const String face = 'assets/face.png';
-  static const String faceWhite = 'assets/face_white.png';
-}
